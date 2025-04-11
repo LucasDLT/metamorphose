7cfa0f71153cf4c327ce5dfea7cca27ffa4f57b8
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-04-11 16:40:09 -0300

    se agrego una animacion personalizada para el pulse sobre el borde de los inputs. Se modifico la altura del Layuout. Finalizada la vista de carga de imagenes

--- metamorphose-ph/src/app/navegacion/(panel)/carga/page.tsx
+++ metamorphose-ph/src/app/navegacion/(panel)/carga/page.tsx
@@ -13,6 +13,7 @@ export default function Carga() {
   const { setFotos, fotos, token, setCategory } = useContext(Context);
   const [error, setError] = useState<IformErrors>({});
   const [selectCategory, setSelectCategory] = useState<boolean>(true);
+  const MAX_HISTORY_LENGTH = 300;
 
   const [formImg, setFormImg] = useState<Ifotos>({
     title: "",
@@ -33,6 +34,11 @@ export default function Carga() {
       ...formImg,
       [event.target.name]: event.target.value,
     });
+    setError((prev) => {
+      const updatedErrors = { ...prev };
+      delete updatedErrors[event.target.name as keyof IformErrors];
+      return updatedErrors;
+    });
   }
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -44,6 +50,11 @@ export default function Carga() {
         url: file,
       });
     }
+    setError((prev) => {
+      const updatedErrors = { ...prev };
+      delete updatedErrors["url"];
+      return updatedErrors;
+    });
   };
 
   const handleActiveChange = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -58,6 +69,11 @@ export default function Carga() {
       });
       setSelectCategory(!selectCategory);
     }
+    setError((prev) => {
+      const updatedErrors = { ...prev };
+      delete updatedErrors["active"];
+      return updatedErrors;
+    });
   };
   const handleCategoryChange = (category: ICategory | null) => {
     setFormImg({
@@ -77,8 +93,40 @@ export default function Carga() {
         name: event.target.value,
       },
     });
+    setError((prev) => {
+      const updatedErrors = { ...prev };
+      delete updatedErrors["category"];
+      return updatedErrors;
+    });
   };
 
+  const handleTextAreaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    if (event.target.value.length > MAX_HISTORY_LENGTH) return;
+
+    event.preventDefault();
+    setFormImg({
+      ...formImg,
+      [event.target.name]: event.target.value,
+    });
+    setError((prev) => {
+      const updatedErrors = { ...prev };
+      delete updatedErrors[event.target.name as keyof IformErrors];
+      return updatedErrors;
+    });
+  };
+  const handleRemoveImage = () => {
+    setFormImg({
+      ...formImg,
+      url: null,
+    });
+    setError((prev) => {
+      const updatedErrors = { ...prev };
+      delete updatedErrors["url"];
+      return updatedErrors;
+    });
+  };
   const handleFile = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -128,7 +176,11 @@ export default function Carga() {
         // Usamos el valor actual del estado `categories` dentro de la función de actualización
         setCategory((prevCategories: ICategory[]) => {
           // Añadimos la categoría si no existe
-          if (!prevCategories.some((category: ICategory) => category.name === formImg.category?.name)) {
+          if (
+            !prevCategories.some(
+              (category: ICategory) => category.name === formImg.category?.name
+            )
+          ) {
             return [...prevCategories, formImg.category as ICategory];
           }
           return prevCategories; // Si ya existe, no la agregamos de nuevo
@@ -159,6 +211,15 @@ export default function Carga() {
       throw new Error("error en el post de imagenes" + error);
     }
   };
+  const getCounterColor = () => {
+    const length = formImg.history?.length;
+    const percentage = (length! / MAX_HISTORY_LENGTH) * 100;
+  
+    if (percentage >= 90) return "text-red-500";
+    if (percentage >= 70) return "text-yellow-400";
+    return "text-gray-400";
+  };
+  
   return (
     <form
       onSubmit={handleFile}
@@ -166,61 +227,80 @@ export default function Carga() {
       method="POST"
     >
       {/*bloque para la imagen */}
-      <div className="grid place-items-center">
-        <h1 className="text-3xl text-center p-2 font-bold text-white drop-shadow-[2px_2px_2px_black]">CARGA DE IMAGENES</h1>
+      <div className="grid place-items-center bg-black/80 p-1">
+        <h1 className="text-3xl text-center font-bold text-white drop-shadow-[2px_2px_2px_black]">
+          CARGA DE IMAGENES
+        </h1>
         <input
-          className="text-gray-300 pl-2 mt-5 background-black "
+          className="text-gray-300 pl-2 mt-1 background-black "
           type="file"
           name="url"
           id="url"
           accept="image/*"
           onChange={handleFileChange}
         />
-        <div
-          
-          style={{ width: "300px", height: "400px" }}
-        >
+        <div style={{ width: "300px", height: "400px", position: "relative" }}>
           {formImg?.url && (
-            <Image 
-            src={URL.createObjectURL(formImg.url)} 
-            alt="preview"       
-            className=" aspect-[1/1] object-cover rounded w-full h-full mt-1  border-opacity-90 shadow-[0_0_20px_5px_rgba(0,0,0,0.8)] hover:shadow-none transition duration-300 ease-in-out"
-            width={500}
-            height={500}/>
+            <>
+              <button
+                type="button"
+                onClick={handleRemoveImage}
+                className="bg-red-600 hover:bg-red-700 text-white text-xs px-2 py-1 rounded shadow absolute top-2 right-1"
+              >
+                X
+              </button>
+              <Image
+                src={URL.createObjectURL(formImg.url)}
+                alt="preview"
+                className=" aspect-[1/1] object-cover rounded w-full h-full mt-1 pb-3 border-opacity-90 shadow-[0_0_20px_5px_rgba(0,0,0,0.8)] hover:shadow-none transition duration-300 ease-in-out"
+                width={500}
+                height={500}
+              />
+            </>
           )}
-      {error.url && <p className="text-red-600 text-center p-1 bg-black/60 mt-40 rounded drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">{error.url}</p>}
-
+          {error.url && (
+            <p className="text-red-600 text-center p-1 bg-black/60 mt-40 rounded drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">
+              {error.url}
+            </p>
+          )}
         </div>
       </div>
       {/*bloque para los datos adicionales */}
-      <div className="flex flex-col items-center h-[450px] w-[300px]  ">
-        <div className="flex flex-col bg-black/80  w-[250px] text-center rounded ">
-        <label htmlFor="title">TITULO</label>
-        <input
-          className="text-white bg-transparent border-b border-gray-100 focus:outline-none animate-pulse"
-          type="text"
-          name="title"
-          id="title"
-          onChange={handleChange}
-          value={formImg.title}
-        />
+      <div className="flex flex-col items-center h-[480px] w-[300px]">
+        <div className="flex flex-col bg-black/80 w-[250px] text-center rounded">
+          <label htmlFor="title">TITULO</label>
+          <input
+            className="text-white bg-transparent border rounded border-gray-100 focus:outline-none pulse-border"
+            type="text"
+            name="title"
+            id="title"
+            onChange={handleChange}
+            value={formImg.title}
+          />
         </div>
-        <div className="flex flex-col bg-black/80 w-[250px] text-center rounded mt-1">
-        <label htmlFor="history">HISTORIA</label>
-        <input
-          type="text"
-          name="history"
-          id="history"
-          onChange={handleChange}
-          value={formImg.history}
-          className="text-white bg-transparent border-b border-gray-100 focus:outline-none animate-pulse "
-        />
+        <div className="flex flex-col bg-black/80 w-[250px] text-center rounded mt-1 ">
+          <label htmlFor="history">HISTORIA</label>
+          <textarea
+            name="history"
+            id="history"
+            onChange={handleTextAreaChange}
+            value={formImg.history}
+            className=" w-[250px] text-white bg-transparent border rounded border-gray-100 focus:outline-none pulse-border "
+            rows={3}
+            cols={20}
+            maxLength={MAX_HISTORY_LENGTH}
+          />
+          <div className={`text-right text-xs mt-1 pr-2 ${getCounterColor()}`}>
+          {formImg.history?.length}/{MAX_HISTORY_LENGTH}
+          </div>
         </div>
         {/* bloque selector para la categoria */}
 
         {selectCategory ? (
           <div className="flex flex-col bg-black/80 h-[100px] w-[250px] text-center p-2 rounded mt-1">
-            <label className="mt-2" htmlFor="category">CREAR CATEGORIA</label>
+            <label className="mt-2" htmlFor="category">
+              CREAR CATEGORIA
+            </label>
             <input
               type="text"
               name="category"
@@ -233,7 +313,7 @@ export default function Carga() {
               onClick={handleActiveChange}
               className="hover:text-gray-500 text-white animate-pulse mt-2"
             >
-              * seleccionar existente
+              * seleccionar existente?
             </button>
           </div>
         ) : (
@@ -247,21 +327,19 @@ export default function Carga() {
                 outline: "none",
                 textAlign: "center",
                 marginTop: "10px",
-                animation: 'pulse 2s infinite'
-                
+                animation: "pulse 2s infinite",
               }}
             />
-              <button
-                onClick={() => setSelectCategory(true)}
-                className="hover:text-gray-500 text-white animate-pulse mt-8"
-              >
-               * crear categoria
-              </button>
+            <button
+              onClick={() => setSelectCategory(true)}
+              className="hover:text-gray-500 text-white animate-pulse mt-8"
+            >
+              * crear categoria?
+            </button>
           </div>
         )}
-            {/* bloque para la fecha y el active*/}
+        {/* bloque para la fecha y el active*/}
         <div className="grid grid-cols-2 text-center mt-1 w-[250px]">
-
           <div className="flex flex-col bg-black/80 rounded mr-1">
             <label htmlFor="createdAt">FECHA</label>
             <input
@@ -301,33 +379,61 @@ export default function Carga() {
           CARGAR
         </button>
         {/* bloque para ver errores*/}
-        <div className="flex flex-col w-[250px] rounded drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] mt-9 ">
-          {error.title && <p className="text-red-500 text-sm p-1 bg-black/70">{error.title}</p>}
+        <div className="flex flex-col w-[250px] rounded drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] mt-1 ">
+          {error.title && (
+            <p className="text-red-500 pl-1 text-sm bg-black/70">
+              {error.title}
+            </p>
+          )}
 
           {error.history && (
-            <p className="text-red-500 text-sm p-1 bg-black/70">{error.history}</p>
+            <p className="text-red-500 pl-1 text-sm bg-black/70">
+              {error.history}
+            </p>
           )}
 
           {error.createdAt && (
-            <p className="text-red-500 text-sm p-1 bg-black/70">{error.createdAt}</p>
+            <p className="text-red-500 pl-1 text-sm bg-black/70">
+              {error.createdAt}
+            </p>
           )}
 
           {error.active && (
-            <p className="text-red-500 text-sm p-1 bg-black/70">{error.active}</p>
+            <p className="text-red-500 pl-1 text-sm bg-black/70">
+              {error.active}
+            </p>
           )}
         </div>
       </div>
-
+      {/* bloque para ver el preview*/}
       <div
         className="
-      border rounded flex flex-col p-4 gap-2 h-[300px] justify-evenly "
+     rounded flex flex-col gap-2 w-[300px] h-[480px] justify-start bg-black/70 overflow-hidden
+  "
       >
-        <p className="text-white rounded">TITULO: {formImg.title}</p>
-        <p className="text-white rounded">
-          CATEGORIA: {formImg.category?.name || "no seleccionada"}
+        <p className="text-3xl text-center font-bold text-white drop-shadow-[2px_2px_2px_black] mb-2">
+          VISTA PREVIA
         </p>
-        <p className="text-white rounded">HISTORIA: {formImg.history}</p>
-        <p className="text-white rounded">FECHA: {formImg.createdAt}</p>
+
+        <div className="text-white text-sm ml-4">
+          <p className="mb-1">
+            <strong>TÍTULO:</strong> {formImg.title}
+          </p>
+          <p className="mb-1">
+            <strong>CATEGORÍA:</strong>{" "}
+            {formImg.category?.name || "no seleccionada"}
+          </p>
+          <p className="mb-1">
+            <strong>FECHA:</strong> {formImg.createdAt}
+          </p>
+
+          <div className="mt-2">
+            <strong>HISTORIA:</strong>
+            <div className="max-h-[180px] overflow-y-auto overflow-x-hidden p-1 mt-1 bg-black/30 rounded text-sm text-white whitespace-pre-wrap break-words">
+              <p className="whitespace-pre-wrap">{formImg.history}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </form>
   );
